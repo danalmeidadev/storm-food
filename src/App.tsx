fbe1517f4a47c102cd5675eed5c764b686778462
danalmeidadev <devdanalmeida@gmail.com>
2021-04-01 23:05:00 -0300

    feat: add commitzen configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {} from '';
 
 const App: React.FC = () => {
   return (
     <View>
-      <Text>Test
+      <Text>Test</Text>
     </View>
   );
 };
 
-export default App;
+export {App};
