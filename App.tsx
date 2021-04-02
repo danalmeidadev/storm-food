a54b9103ccd97baa98e7c30354fec3bdcfa06979
danalmeidadev <devdanalmeida@gmail.com>
2021-04-01 22:01:29 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {} from '';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+};
+
+export default App;
